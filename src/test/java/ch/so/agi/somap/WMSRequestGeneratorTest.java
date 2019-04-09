68a1b4839212740987d6c50ed083464188d72c99
Stefan Ziegler <edi.gonzales@gmail.com>
2019-04-09 19:32:40 +0200

    within

--- src/test/java/ch/so/agi/somap/WMSRequestGeneratorTest.java
+++ src/test/java/ch/so/agi/somap/WMSRequestGeneratorTest.java
@@ -5,7 +5,7 @@ import java.io.File;
 import org.junit.jupiter.api.Test;
 
 public class WMSRequestGeneratorTest {
-    @Test 
+    //@Test 
     public void createRequestsFromBoundingBox_Ok() {
         WMSRequestGenerator requestGenerator = new WMSRequestGenerator();
         requestGenerator.createCSV(1000, 0.01, 20, "1920,1080", "2560,1440", "2590000,1210000,2650000,1270000");
